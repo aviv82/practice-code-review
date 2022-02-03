470a5ec3bb45052fb0272acb95d9626bb065419d
Aviv <aviv.dror@gmail.com>
2022-02-03 03:49:37 +0100

    setup tests

--- src/intersection/solution-intersection.spec.js
+++ src/intersection/solution-intersection.spec.js
@@ -1,3 +1,11 @@
-import { intersection } from './solution-intersection.js';
+import { firstTry } from './aviv/sandbox.js';
 
-intersection(7);
+for (const solution of [firstTry]) {
+  describe('returns new array of intersecting values', () => {
+    describe('default parameters', () => {
+      it('default parameters return an empty array', () => {
+        expect(solution).toEqual([]);
+      });
+    });
+  });
+}
